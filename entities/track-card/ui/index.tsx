3c659d3e8c2e8ca9c54704202f661900a36d40df
Kira Burova <burova@talon.one>
2022-10-01 14:28:29 +0200

    added player for audio preview

--- entities/track-card/ui/index.tsx
+++ entities/track-card/ui/index.tsx
@@ -3,12 +3,19 @@ import { Track } from '@shared/models';
 import { secondsToMinutes } from '@shared/lib';
 
 import styles from './styles.module.scss';
+import { useEffect, useRef } from 'react';
 
 type TracklistCardProps = {
   track: Track;
 };
 
 const TracklistCard = ({ track }: TracklistCardProps) => {
+  const audioRef = useRef<HTMLAudioElement>();
+
+  useEffect(() => {
+    audioRef.current.volume = 0.1;
+  }, []);
+
   return (
     <div className={styles.card}>
       <img src={track.album.cover_medium} />
@@ -16,6 +23,10 @@ const TracklistCard = ({ track }: TracklistCardProps) => {
         {track.title} ({track.album.title})
         {track.explicit_lyrics && <span className={styles.explicit}>E</span>}
         <div>{secondsToMinutes(track.duration)}</div>
+        <audio controls ref={audioRef} className={styles.audioPlayer}>
+          <source src={track.preview} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
       </div>
     </div>
   );
