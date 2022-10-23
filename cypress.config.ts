d5f35ba2c2e39d48874e24f0fb80cc7f6218befc
Kira Burova <burova@talon.one>
2022-10-23 13:20:21 +0200

    update webflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'cypress';
+let baseUrl = process.env.CI
+  ? 'http://staging.example.com'
+  : 'https://kiraburova.github.io/music-app/';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: baseUrl,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
